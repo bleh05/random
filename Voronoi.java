b66a10b419b492f546a2650b89088b360a71c6c4
bleh0.5 <qinxiaoying@gmail.com>
2019-10-22 23:03:06 -0500

    Update Voronoi.java

--- Voronoi.java
+++ Voronoi.java
@@ -1,4 +1,11 @@
-
+/** FRAMEWORK
+ * Find all lines perpendicular to the line segment from point x to all other points at the midpoint of x and a.
+ * then, you can check if each line segment intersects another line, if it does, then it should not be used in the area calc
+ * find all the valid segments, and look for all of the intersections. 
+ * Each intersection can be shown as a vertex as for a triangle, and calculate all the areas of the triangles (height will be midpoint of
+ * base point to point i, and then length is distance between the two intersections.
+ * do that for all triangles, yielding an n^3 algorithm
+ **/
 public class Voronoi
 {
  public static double[] voronoi_areas(Point[] p)
